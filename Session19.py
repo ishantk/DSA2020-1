0cdc5bb37dfd0b4a91d628d7f97162ea0d9b95b0
Ishant Kumar <er.ishant@gmail.com>
2020-03-02 17:42:54 +0530

    Session19 - Problem Statement with Hint

--- Session19.py
+++ Session19.py
@@ -9,8 +9,14 @@ frequency1 = 0
 # We need to find frequency of occurrence of word institution in above 5 reviews
 
 # 1. Put the data in an effective data structures
+#    > HashTable | Capacity -> number of Words in number of Reviews
 # 2. Implement an Algorithm to calculate frequency
+#    > Whenever a collision will occur we will increment the count
 # 3. Implementation of OOPS with Data Structures is preferable :)
+#    > Word : word, frequency, alphabets
 
 
 print(">> {} occurs {} times".format(word1, frequency1))
+
+
+
