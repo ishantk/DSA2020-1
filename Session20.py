0678c4e10a32a7d8143a48a81a7512f579493547
Ishant Kumar <er.ishant@gmail.com>
2020-03-09 17:57:40 +0530

    Session20 - HeapSort Solution Part-II

--- Session20.py
+++ Session20.py
@@ -64,11 +64,14 @@ def heapify(numbers, n, i):
         maxNumIdx = rightIdx
 
     # Swap The Elements to Build the Max Heap
+    # Root Element shall be Swapped with largest
+    # From there onwards we continue heapify :)
     if maxNumIdx != i:
         print("[maxNumIdx:{} != i:{}]".format(maxNumIdx, i))
         numbers[i], numbers[maxNumIdx] = numbers[maxNumIdx], numbers[i]
+
         # Recursion
-        print("[Recursion]: heapify numbers:{} | n:{} | maxNumIdx:{}"
+        print("[Recursion From Heapify]: heapify numbers:{} | n:{} | maxNumIdx:{}"
               .format(numbers, n, maxNumIdx))
         heapify(numbers, n, maxNumIdx)
 
@@ -81,6 +84,20 @@ def heapSort(numbers):
     # Building Max Heap for numbers:
     for i in range(n, 0, -1):
         heapify(numbers, n, i)
+
+    for i in range(n-1, 0, -1):
+        print("SWAP: {}->{} with {}->{} in numbers:{}"
+              .format(i, numbers[i], 0, numbers[0], numbers))
+        # HeapSort: Swap the 0th i.e. Root
+        numbers[i], numbers[0] = numbers[0], numbers[i]
+
+        print("SWAP: new numbers:{}".format(numbers))
+
+        # Heapifying the Root Element
+        # Recursion
+        print("[Recursion From Heapify]: heapify numbers:{} | i:{} | Root:{}"
+              .format(numbers, i, 0))
+        heapify(numbers, i, 0)
 
 
 numbers = [91, 76, 22, 11, 21, 34, 66]
