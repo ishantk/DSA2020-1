e77206d4f2d4ece72a1a589d63885cfff163ee8d
Ishant Kumar <er.ishant@gmail.com>
2020-03-12 18:03:14 +0530

    Session22 - QuickSort Algo Part-II

--- Session22.py
+++ Session22.py
@@ -33,6 +33,58 @@
 
     numbers: 10, 33, 88, 99, 44, 57, 76
 
+    .
+    ..
+    ....
 
+    when j:5
+    numbers : 10, 33, 44, 57, 88, 99, 76
+
+    Once Loop is finished, j shall be len-1 i.e. 6
+    Here, we will place pivot at correct position by doing a swap
+    on numbers[i+1] with numbers[len-1]
+
+    when j:6 | swapping 88 and 76
+    numbers : 10, 33, 44, 57, 88, 99, 76 -> numbers : 10, 33, 44, 57, 76, 99, 88
+    76 is in the right position :)
 
 """
+
+def partition(numbers, low, high):
+
+    pivot = numbers[high]
+    i = low - 1
+
+    print("PARTITION START - NUMBERS:{} | LOW:{} |  HIGH:{}".format(numbers, low, high))
+    print("PIVOT:{}".format(pivot))
+
+    # Iterate for j = 0 to len -1
+    for j in range(low, high):
+        if numbers[j] <= pivot:
+            i += 1
+            # Swap Numbers at i and j
+            numbers[i], numbers[j] = numbers[j], numbers[i]
+
+    # Once Loop terminates, we shall place pivot at correct position
+    numbers[i+1], numbers[high] = numbers[high], numbers[i+1]
+
+    print("PARTITION FINISH - NUMBERS:{} | LOW:{} |  HIGH:{} | i:{}".format(numbers, low, high, i))
+
+    return i+1
+
+
+def quickSort(numbers, low, high):
+
+    partition(numbers, low, high)
+
+
+
+
+print(">> Original Numbers before Quick Sort:")
+numbers= [10, 88, 33, 99, 44, 57, 76]
+print(numbers)
+
+quickSort(numbers, 0, len(numbers)-1)
+
+print(">> Sorted Numbers after Quick Sort:")
+print(numbers)
